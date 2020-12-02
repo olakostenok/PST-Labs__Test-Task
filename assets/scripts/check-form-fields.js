85d5b43e380d1443c3752a454b745f5a2c5a0dbe
Olya Kostenok <ola.kostenok@gmail.com>
2020-12-02 20:21:14 +0300

    feat: add validation fields and redirect to news page

---
+++ assets/scripts/check-form-fields.js
@@ -0,0 +1,59 @@
+const logInButton = document.querySelector('.login-form .button');
+const logInInputs = document.querySelectorAll('.login-form input');
+let validField = 0;
+
+function resetFields() {
+  setTimeout(() => {
+    [...logInInputs].forEach((el) => {
+      el.value = '';
+      el.classList.remove('valid-field');
+      el.classList.remove('invalid-field');
+    });
+  }, 2000);
+}
+function checkFormFields() {
+  const user = {
+    login: 'user',
+    password: 'user',
+  };
+  const admin = {
+    login: 'admin',
+    password: 'admin',
+  };
+
+  [...logInInputs].forEach((el, index) => {
+    if (index == 0) {
+      if (el.value == user.login) {
+        el.classList.add('valid-field');
+        validField++;
+        localStorage.setItem('login as', user.login);
+      } else {
+        el.classList.add('invalid-field');
+      }
+    }
+    if (index == 1) {
+      if (el.value == user.password) {
+        el.classList.add('valid-field');
+        validField++;
+      } else {
+        el.classList.add('invalid-field');
+      }
+    }
+  });
+
+  if (validField == logInInputs.length) {
+    setTimeout(function () {
+      window.location.href = 'news-page.html';
+    }, 1000);
+  } else {
+    resetFields();
+  }
+  validField = 0;
+}
+
+logInButton.addEventListener('click', () => {
+  checkFormFields();
+  resetFields();
+});
+
+// export default checkFormFields;
--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -1,5 +1,6 @@
 
 import tabSwitcher from './tabs-switcher.js';
+// import checkFormFields from './check-form-fields.js';
 
 function init () {
     tabSwitcher ();
